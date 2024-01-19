fe218a86a147f82562eabf24e0a5e71dcd930ca9
Abahi99 <bahiayoub021@gmail.com>
2024-01-19 11:05:35 -0500

    task0

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -2,9 +2,12 @@
 
 #function that removes all characters c and C from a string.
 def no_c(my_string):
+    arr = []
 
-    for i in range(len(my_string)):
-        if my_string[i] == "c" or my_string[i] == "C":
-            my_string[i] = ""
-    return my_string
+    for char in my_string:
+        if char != "c" and char != "C":
+            arr.append(char)
+    string = ''.join(str(x) for x in arr)
+    return string
+
 
