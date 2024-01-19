b3145869dfadfa356d0b8f15e8b6e119427489b1
Abahi99 <bahiayoub021@gmail.com>
2024-01-19 14:26:17 -0500

    task3

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -3,19 +3,12 @@
 #Write a function that returns a key with the biggest integer value.
 
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if a_dictionary:
+        max_key = max(a_dictionary, key=a_dictionary.get)
+        return max_key
+    else:
         return None
-    
-    value = a_dictionary.values()
-    score = 0
-    for val in value:
-        if score < val:
-            score = val
-    key = ""
-    for item in a_dictionary.keys():
-        if a_dictionary[item] == score:
-            key = a_dictionary[item]
-    return key
+
 
 
 
