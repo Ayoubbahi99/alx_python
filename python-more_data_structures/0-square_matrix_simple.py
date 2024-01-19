7e37a91bb01aab7fe02b61953293b89c2bc99f8c
Abahi99 <bahiayoub021@gmail.com>
2024-01-19 13:54:19 -0500

    task0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,10 @@
 
 #Write a function that computes the square value of all integers of a matrix.
 def square_matrix_simple(matrix=[]):
+    new_matrix = []
     for arr in matrix:
+        array = []
         for num in arr:
-            num = num ** 2
-    return matrix
+            array.append(num ** 2)
+        new_matrix.append(array)
+    return new_matrix
