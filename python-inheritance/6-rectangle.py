7b282a19b9927227c24a236271088a09c7aa7a60
Abahi99 <bahiayoub021@gmail.com>
2024-01-23 13:51:25 -0500

    task6

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -2,7 +2,11 @@
 """Defines a class BaseGeometry based on 5-base_geometry.py"""
 
 
-class BaseGeometry:
+
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
     """Class BaseGeometry.
     """
 
@@ -17,28 +21,3 @@ class BaseGeometry:
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-        
-    def area(self):
-        """Area function.
-
-        Raises:
-            Exception: if area is not implemented.
-        """
-        raise Exception("area() is not implemented")
-    
-    def integer_validator(self, name, value):
-        """Validate the input value.
-
-        Args:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
