a53d7b668dc170877ffebf5fb4aa46430dc64a1b
Abahi99 <bahiayoub021@gmail.com>
2024-01-25 18:34:58 -0500

    task1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """Import Base class from the base file"""
-Base = __import__('base').Base
+from models.base import Base
 
 """Defines a class Rectangle based on base.py"""
 class Rectangle(Base):    
