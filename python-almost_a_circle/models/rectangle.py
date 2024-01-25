b51954506ccc3ccd7d0dc5a13210f7061efdbce1
Abahi99 <bahiayoub021@gmail.com>
2024-01-25 18:33:44 -0500

    task1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle based on base.py"""
 
+"""Import Base class from the base file"""
 Base = __import__('base').Base
 
+"""Defines a class Rectangle based on base.py"""
 class Rectangle(Base):    
 
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -21,43 +22,50 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
-
+    """Getter method for width."""
     @property
     def width(self):
-        """Getter method for width."""
+        """Return the width of rectangle."""
         return self.__width
     
+    """setter method for width."""   
     @width.setter
     def width(self, width):
-        """setter method for width."""
+        """set width of rectangle to a new one."""
         self.__width = width
     
+    """Getter method for height."""
     @property
     def height(self):
-        """Getter method for height."""
+        """Get the height of rectangle."""
         return self.__height
     
+    """setter method for height."""
     @height.setter
     def width(self, height):
-        """setter method for height."""
+        """set the height of a new value."""
         self.__height = height
     
+    """Getter method for x."""
     @property
     def x(self):
-        """Getter method for x."""
+        """Get the x value."""
         return self.__x
     
+    """setter method for x."""
     @x.setter
     def x(self, x):
-        """setter method for x."""
+        """set the x value to a new one."""
         self.__x = x
 
+    """Getter method for y."""
     @property
     def y(self):
-        """Getter method for y."""
+        """Get the y value."""
         return self.__y
 
+    """setter method for y."""
     @y.setter
     def width(self, y):
-        """setter method for y."""
+        """set the y value to a new one."""
         self.__y = y
