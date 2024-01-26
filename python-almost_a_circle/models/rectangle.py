9175b1f57d534d587303dd6a7847eff6e70fa07d
Abahi99 <bahiayoub021@gmail.com>
2024-01-26 11:42:30 -0500

    task3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,118 +1,144 @@
 #!/usr/bin/python3
+"""Defines a class Rectangle that inherits from Base"""
 
-"""Import Base class from the base file"""
+
 from models.base import Base
 
-"""Defines a class Rectangle based on base.py"""
+
 class Rectangle(Base):
-    """Defines a class Rectangle inherit from base.py file class Base
-        create methods for all the instance attributes 
+    """Class that defines properties of Rectangle.
+
+     Attributes:
+        width (int): width of rectangle.
+        height (int): height of rectangle.
+        x (int): x.
+        y (int): y.
     """
-  
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Creates new instances of Rectangle.
+        """Creates new instances of rectangle.
 
         Args:
-            id (int, optional): Identity of each instance. Getting from Base class.
-            width (int): Give the width of each rectangle.
-            height (int): Give the height of each rectangle.
-            x (int): Give the place of the rectangle.
-            y (int): Give the place of the rectangle.
+            width (int): width of rectangle.
+            height (int): height of rectangle.
+            x (int, optional): x. Defaults to 0.
+            y (int, optional): y. Defaults to 0.
+            id (int, optional): Identity number of rectangle. Defaults to None.
         """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
 
-    """Getter method for width."""
     @property
     def width(self):
-        """Return the width of rectangle."""
-        return self.__width
-    
-    """setter method for width."""   
-    @width.setter
-    def width(self, width):
-        """set width of rectangle to a new one.
-            
-            Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than 0.
+        """Width retriever.
+
+        Returns:
+            int: width of rectangle.
         """
-        if not isinstance(width, int):
-            raise TypeError("{} must be an integer".format(width))
-        if width < 0:
-            raise ValueError("{} must be greater than 0".format(width))
-        self.__width = width
-    
-    """Getter method for height."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Property setter for width of rectangle.
+
+        Args:
+            value (int): width of rectangle.
+
+        Raises:
+            TypeError: if width is not an integer.
+            ValueError: if width is less than or equal to zero.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
     @property
     def height(self):
-        """Get the height of rectangle."""
-        return self.__height
-    
-    """setter method for height."""
-    @height.setter
-    def height(self, height):
-        """set the height of a new value.
-            
-            Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than 0.
+        """Height retriever.
+
+        Returns:
+            int: height of rectangle.
         """
-        if not isinstance(height, int):
-            raise TypeError("{} must be an integer".format(height))
-        if height < 0:
-            raise ValueError("{} must be greater than 0".format(height))
-        self.__height = height
-    
-    """Getter method for x."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Property setter for height of rectangle.
+
+        Args:
+            value (int): height of rectangle.
+
+        Raises:
+            TypeError: if height is not an integer.
+            ValueError: if height is less than or equal to zero.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
     @property
     def x(self):
-        """Get the x value."""
-        return self.__x
-    
-    """setter method for x."""
-    @x.setter
-    def x(self, x):
-        """set the x value to a new one.
-            
-            Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-        """
-        if not isinstance(x, int):
-            raise TypeError("{} must be an integer".format(x))
-        if x <= 0:
-            raise ValueError("{} must be greater than 0".format(x))        
-        self.__x = x
+        """x retriever.
 
-    """Getter method for y."""
+        Returns:
+            int: x.
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Property setter for x.
+
+        Args:
+            value (int): x.
+
+        Raises:
+            TypeError: if x is not an integer.
+            ValueError: if x is less than or equal to zero.
+        """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
     @property
     def y(self):
-        """Get the y value."""
+        """y retriever.
+
+        Returns:
+            int: y.
+        """
         return self.__y
 
-    """setter method for y."""
     @y.setter
-    def y(self, y):
-        """set the y value to a new one.
+    def y(self, value):
+        """Property setter for y.
 
-            Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
+        Args:
+            value (int): y.
+
+        Raises:
+            TypeError: if y is not an integer.
+            ValueError: if y is less than or equal to zero.
         """
-        if not isinstance(y, int):
-            raise TypeError("{} must be an integer".format(y))
-        if y <= 0:
-            raise ValueError("{} must be greater than 0".format(y))
-        self.__y = y
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
 
-
-    """The area of each rectangle"""
     def area(self):
-        """Multiply height with the width to get the area."""
-        return self.__height * self.__width
+        """Calculates area of a rectangle.
 
+        Returns:
+            int: area.
+        """
+        return self.__width * self.__height
