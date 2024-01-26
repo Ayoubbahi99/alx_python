e3fe89b210aa6089006982c007a8b5d3d09ce5ae
Abahi99 <bahiayoub021@gmail.com>
2024-01-26 13:02:49 -0500

    task4

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -153,4 +153,4 @@ class Rectangle(Base):
         for i in range(self.__height):
             for j in range(self.__width):
                 print("#", end="")
-        print()
+            print()
