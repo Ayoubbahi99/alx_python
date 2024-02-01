74e050f3658bd328a263b14ac18c4d588ab1084d
Abahi99 <bahiayoub021@gmail.com>
2024-02-01 14:50:31 -0500

    task2

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""import sys and requests"""
+import sys
+import requests
+
+"""
+Write a Python script that takes in a URL and an email address, 
+sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.
+"""
+email = sys.argv[2]
+data = {'email': email}
+r = requests.post(sys.argv[1], data=data)
+print(f"Your email is: {sys.argv[2]}")
